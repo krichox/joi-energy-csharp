36dfb0c13530305a7b5d7c711a0997c0bda1731b
konglinghan <krichox@gmail.com>
2021-03-26 14:42:22 +0800

    Optimize data initialization

--- JOIEnergy/Startup.cs
+++ JOIEnergy/Startup.cs
@@ -6,6 +6,7 @@ using JOIEnergy.Domain;
 using JOIEnergy.Enums;
 using JOIEnergy.Generator;
 using JOIEnergy.Services;
+using JOIEnergy.WebOS;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -28,35 +29,9 @@ namespace JOIEnergy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var readings =
-                GenerateMeterElectricityReadings();
-
-            var pricePlans = new List<PricePlan> {
-                new PricePlan{
-                    EnergySupplier = Enums.Supplier.DrEvilsDarkEnergy,
-                    UnitRate = 10m,
-                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
-                },
-                new PricePlan{
-                    EnergySupplier = Enums.Supplier.TheGreenEco,
-                    UnitRate = 2m,
-                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
-                },
-                new PricePlan{
-                    EnergySupplier = Enums.Supplier.PowerForEveryone,
-                    UnitRate = 1m,
-                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
-                }
-            };
-
             services.AddMvc();
+            services.InitJOIEnergyDatas();
             services.AddDataService();
-            //services.AddTransient<IAccountService, AccountService>();
-            //services.AddTransient<IMeterReadingService, MeterReadingService>();
-            //services.AddTransient<IPricePlanService, PricePlanService>();
-            services.AddSingleton((IServiceProvider arg) => readings);
-            services.AddSingleton((IServiceProvider arg) => pricePlans);
-            services.AddSingleton((IServiceProvider arg) => SmartMeterToPricePlanAccounts);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,46 +44,7 @@ namespace JOIEnergy
 
             app.UseMvc();
         }
-
-        private Dictionary<string, List<ElectricityReading>> GenerateMeterElectricityReadings() {
-            var readings = new Dictionary<string, List<ElectricityReading>>();
-            var generator = new ElectricityReadingGenerator();
-            var smartMeterIds = SmartMeterToPricePlanAccounts.Select(mtpp => mtpp.Key);
-
-            foreach (var smartMeterId in smartMeterIds)
-            {
-                readings.Add(smartMeterId, generator.Generate(20));
-            }
-            return readings;
-        }
-
-        public Dictionary<String, Supplier> SmartMeterToPricePlanAccounts
-        {
-            get
-            {
-                Dictionary<String, Supplier> smartMeterToPricePlanAccounts = new Dictionary<string, Supplier>();
-                smartMeterToPricePlanAccounts.Add("smart-meter-0", Supplier.DrEvilsDarkEnergy);
-                smartMeterToPricePlanAccounts.Add("smart-meter-1", Supplier.TheGreenEco);
-                smartMeterToPricePlanAccounts.Add("smart-meter-2", Supplier.DrEvilsDarkEnergy);
-                smartMeterToPricePlanAccounts.Add("smart-meter-3", Supplier.PowerForEveryone);
-                smartMeterToPricePlanAccounts.Add("smart-meter-4", Supplier.TheGreenEco);
-                return smartMeterToPricePlanAccounts;
-            }
-
-
-
-            //用户 智能电表ID 电力供应商
-            //莎拉 smart-meter-0	邪恶博士的黑暗能量
-            //彼德 smart-meter-1	绿色生态
-            //查理 smart-meter-2	邪恶博士的黑暗能量
-            //安德里亚 smart-meter-3	每个人的力量
-            //亚历克斯 smart-meter-4	绿色生态
-
-            //Sarah smart-meter-0	Dr Evil's Dark Energy
-            //Peter smart-meter-1	The Green Eco
-            //Charlie smart-meter-2	Dr Evil's Dark Energy
-            //Andrea smart-meter-3	Power for Everyone
-            //Alex smart-meter-4	The Green Eco
-        }
     }
+
+       
 }
---
+++ JOIEnergy/WebOS/InitClassDatas.cs
@@ -0,0 +1,90 @@
+﻿using JOIEnergy.Domain;
+using JOIEnergy.Enums;
+using JOIEnergy.Generator;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JOIEnergy.WebOS
+{
+    /// <summary>
+    ///  初始化数据
+    /// </summary>
+    public static class InitClassDatas
+    {
+        public static IServiceCollection InitJOIEnergyDatas(this IServiceCollection services)
+        {
+            var readings =
+                GenerateMeterElectricityReadings();
+            List<PricePlan> pricePlans = GeneratePricePlan();
+            services.AddSingleton((IServiceProvider arg) => readings);
+            services.AddSingleton((IServiceProvider arg) => pricePlans);
+            services.AddSingleton((IServiceProvider arg) => SmartMeterToPricePlanAccounts);
+            return services;
+        }
+
+        private static List<PricePlan> GeneratePricePlan()
+        {
+            return new List<PricePlan> {
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.DrEvilsDarkEnergy,
+                    UnitRate = 10m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                },
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.TheGreenEco,
+                    UnitRate = 2m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                },
+                new PricePlan{
+                    EnergySupplier = Enums.Supplier.PowerForEveryone,
+                    UnitRate = 1m,
+                    PeakTimeMultiplier = new List<PeakTimeMultiplier>()
+                }
+            };
+        }
+
+        private static Dictionary<string, List<ElectricityReading>> GenerateMeterElectricityReadings()
+        {
+            var readings = new Dictionary<string, List<ElectricityReading>>();
+            var generator = new ElectricityReadingGenerator();
+            var smartMeterIds = SmartMeterToPricePlanAccounts.Select(mtpp => mtpp.Key);
+
+            foreach (var smartMeterId in smartMeterIds)
+            {
+                readings.Add(smartMeterId, generator.Generate(20));
+            }
+            return readings;
+        }
+
+        public static Dictionary<String, Supplier> SmartMeterToPricePlanAccounts
+        {
+            get
+            {
+                Dictionary<String, Supplier> smartMeterToPricePlanAccounts = new Dictionary<string, Supplier>();
+                smartMeterToPricePlanAccounts.Add("smart-meter-0", Supplier.DrEvilsDarkEnergy);
+                smartMeterToPricePlanAccounts.Add("smart-meter-1", Supplier.TheGreenEco);
+                smartMeterToPricePlanAccounts.Add("smart-meter-2", Supplier.DrEvilsDarkEnergy);
+                smartMeterToPricePlanAccounts.Add("smart-meter-3", Supplier.PowerForEveryone);
+                smartMeterToPricePlanAccounts.Add("smart-meter-4", Supplier.TheGreenEco);
+                return smartMeterToPricePlanAccounts;
+            }
+
+            //用户 智能电表ID 电力供应商
+            //莎拉 smart-meter-0	邪恶博士的黑暗能量
+            //彼德 smart-meter-1	绿色生态
+            //查理 smart-meter-2	邪恶博士的黑暗能量
+            //安德里亚 smart-meter-3	每个人的力量
+            //亚历克斯 smart-meter-4	绿色生态
+
+            //Sarah smart-meter-0	Dr Evil's Dark Energy
+            //Peter smart-meter-1	The Green Eco
+            //Charlie smart-meter-2	Dr Evil's Dark Energy
+            //Andrea smart-meter-3	Power for Everyone
+            //Alex smart-meter-4	The Green Eco
+        }
+    }
+}
+
